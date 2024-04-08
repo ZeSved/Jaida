ff880361916b7190884bb3b2adfbb61c9f940396
Timothy Korell <timothy.korell@gmail.com>
2024-04-08 12:26:55 +0200

    added header and scss

---
+++ app/_components/Header/page.tsx
@@ -0,0 +1,19 @@
+import Image from 'next/image'
+import Jaida_logo from '@/public/Jaida Logo size 1.svg'
+import s from './header.module.scss'
+
+export default function Header() {
+	return (
+		<header
+			id='header'
+			className={s.header}>
+			<Image
+				src={Jaida_logo}
+				alt='Jaida Logo'
+			/>
+			<h1>Jaida</h1>
+			<span></span>
+			<div></div>
+		</header>
+	)
+}
