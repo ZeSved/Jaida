1f53b3cf63eeac679327f451f1a4423304f21bb5
Timothy Korell <timothy.korell@gmail.com>
2024-04-24 14:27:18 +0200

    progress on input component

--- app/m/[id]/settings/Input.tsx
+++ app/m/[id]/settings/Input.tsx
@@ -1,1 +1,47 @@
-export default function Input() {}
+'use client'
+
+export default function Input({ mode }: { mode: 'link' | 'image' }) {
+	return (
+		<div>
+			{mode === 'image' ? (
+				<>
+					<InputField inputType='file' />
+					<p>or</p>
+					<InputField
+						inputType='url'
+						text='Image Url'
+					/>
+				</>
+			) : (
+				<>
+					<InputField
+						inputType='text'
+						text='Link Text'
+					/>
+					<InputField
+						inputType='url'
+						text='Link Url'
+					/>
+				</>
+			)}
+		</div>
+	)
+}
+
+function InputField({ text, inputType }: { text?: string; inputType: 'url' | 'text' | 'file' }) {
+	return inputType !== 'file' ? (
+		<fieldset>
+			<legend>{text}</legend>
+			<input
+				type={inputType}
+				placeholder={text}
+				pattern={inputType === 'url' ? 'https://.*' : ''}
+			/>
+		</fieldset>
+	) : (
+		<input
+			type={inputType}
+			accept='image/*'
+		/>
+	)
+}
