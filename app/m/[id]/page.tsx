6767a6077bbc483502f47f71b54f98d209a0908b
Timothy Korell <timothy.korell@gmail.com>
2024-05-13 12:30:34 +0200

    added more replacements

--- app/m/[id]/page.tsx
+++ app/m/[id]/page.tsx
@@ -5,234 +5,32 @@ import Settings from './settings/Settings'
 import { User, onAuthStateChanged } from 'firebase/auth'
 import { auth, db } from '@/firebase/firebase'
 import {
-	getDocs,
-	collection,
 	doc,
 	getDoc,
 	DocumentSnapshot,
 	DocumentData,
-	QuerySnapshot,
 	updateDoc,
 	DocumentReference,
 } from 'firebase/firestore'
-import { Metadata } from 'next'
-import { title } from 'process'
+import { replacements } from './replacements'
 
-const replacements = {
-	alpha: {
-		active: true,
-		options: [
-			{ text: 'Α', active: true },
-			{ text: 'α', active: false },
-		],
-	},
-	beta: {
-		active: true,
-		options: [
-			{ text: 'Β', active: true },
-			{ text: 'β', active: false },
-		],
-	},
-	gamma: {
-		active: true,
-		options: [
-			{ text: 'Γ', active: true },
-			{ text: 'γ', active: false },
-		],
-	},
-	delta: {
-		active: true,
-		options: [
-			{ text: 'Δ', active: true },
-			{ text: 'δ', active: false },
-		],
-	},
-	epsilon: {
-		active: true,
-		options: [
-			{ text: 'Ε', active: true },
-			{ text: 'ε', active: false },
-		],
-	},
-	zeta: {
-		active: true,
-		options: [
-			{ text: 'Ζ', active: true },
-			{ text: 'ζ', active: false },
-		],
-	},
-	eta: {
-		active: true,
-		options: [
-			{ text: 'Η', active: true },
-			{ text: 'η', active: false },
-		],
-	},
-	theta: {
-		active: true,
-		options: [
-			{ text: 'Θ', active: true },
-			{ text: 'θ', active: false },
-		],
-	},
-	iota: {
-		active: true,
-		options: [
-			{ text: 'Ι', active: true },
-			{ text: 'ι', active: false },
-		],
-	},
-	kappa: {
-		active: true,
-		options: [
-			{ text: 'Κ', active: true },
-			{ text: 'κ', active: false },
-		],
-	},
-	lambda: {
-		active: true,
-		options: [
-			{ text: 'Λ', active: true },
-			{ text: 'λ', active: false },
-		],
-	},
-	mu: {
-		active: true,
-		options: [
-			{ text: 'Μ', active: true },
-			{ text: 'μ', active: false },
-		],
-	},
-	nu: {
-		active: true,
-		options: [
-			{ text: 'Ν', active: true },
-			{ text: 'ν', active: false },
-		],
-	},
-	xi: {
-		active: true,
-		options: [
-			{ text: 'Ξ', active: true },
-			{ text: 'ξ', active: false },
-		],
-	},
-	omicron: {
-		active: true,
-		options: [
-			{ text: 'Ο', active: true },
-			{ text: 'ο', active: false },
-		],
-	},
-	pi: {
-		active: true,
-		options: [
-			{ text: 'Π', active: true },
-			{ text: 'π', active: false },
-		],
-	},
-	rho: {
-		active: true,
-		options: [
-			{ text: 'Ρ', active: true },
-			{ text: 'ρ', active: false },
-		],
-	},
-	sigma: {
-		active: true,
-		options: [
-			{ text: 'Σ', active: true },
-			{ text: 'σ', active: false },
-		],
-	},
-	tau: {
-		active: true,
-		options: [
-			{ text: 'Τ', active: true },
-			{ text: 'τ', active: false },
-		],
-	},
-	upsilon: {
-		active: true,
-		options: [
-			{ text: 'Υ', active: true },
-			{ text: 'υ', active: false },
-		],
-	},
-	phi: {
-		active: true,
-		options: [
-			{ text: 'Φ', active: true },
-			{ text: 'φ', active: false },
-		],
-	},
-	chi: {
-		active: true,
-		options: [
-			{ text: 'Χ', active: true },
-			{ text: 'χ', active: false },
-		],
-	},
-	psi: {
-		active: true,
-		options: [
-			{ text: 'Ψ', active: true },
-			{ text: 'ψ', active: false },
-		],
-	},
-	omega: {
-		active: true,
-		options: [
-			{ text: 'Ω', active: true },
-			{ text: 'ω', active: false },
-		],
-	},
-}
+// function reducer(rep: typeof replacements, action: Action) {
+// 	return {
+// 		...rep,
+// 		[action.type as Replacements]: action.payload,
+// 	}
+// }
 
-const names = [
-	'alpha',
-	'beta',
-	'gamma',
-	'delta',
-	'epsilon',
-	'zeta',
-	'eta',
-	'theta',
-	'iota',
-	'kappa',
-	'lambda',
-	'mu',
-	'nu',
-	'xi',
-	'omicron',
-	'pi',
-	'rho',
-	'sigma',
-	'tau',
-	'upsilon',
-	'phi',
-	'chi',
-	'psi',
-	'omega',
-] as const
-
-function reducer(rep: typeof replacements, action: Action) {
-	return {
-		...rep,
-		[action.type as Replacements]: action.payload,
-	}
-}
-
-type Action = {
-	type: `${Replacements}`
-	payload: {
-		active: boolean
-		options: [{ text: string; active: boolean }, { text: string; active: boolean }]
-	}
-}
+// type Action = {
+// 	type: `${Replacements}`
+// 	payload: {
+// 		active: boolean
+// 		options: [{ text: string; active: boolean }, { text: string; active: boolean }]
+// 	}
+// }
 
 export default function MarkdownEditor() {
-	const [rep, dispatch] = useReducer(reducer, replacements)
+	// const [rep, dispatch] = useReducer(reducer, replacements)
 	const [bg, setBg] = useState<CSSProperties>({ background: '1a1a1a' })
 	const [currentDoc, setCurrentDoc] = useState<
 		DocumentSnapshot<DocumentData, DocumentData> | undefined
@@ -291,15 +89,15 @@ export default function MarkdownEditor() {
 
 		if (e.key === ' ') {
 			let replaced = false
-			for (let i = 0; i < names.length; i++) {
-				const r = rep[`${names[i]}`]
+			for (let i = 0; i < replacements.length; i++) {
+				const rep = replacements[i]
 
-				if (ref.current!.textContent?.includes(names[i])) replaced = true
+				if (ref.current!.textContent?.includes(rep.replaceWith)) replaced = true
 
-				if (!r.active) continue
+				if (!rep.active) continue
 				const newString = ref.current!.textContent!.replace(
-					new RegExp(names[i], 'gi'),
-					r.options.find((t) => t.active === true)!.text
+					new RegExp(rep.replaceWith, 'gi'),
+					rep.options ? rep.options.find((t) => t.active === true)!.text : rep.option
 				)
 				ref.current!.textContent = newString
 			}
@@ -329,6 +127,7 @@ export default function MarkdownEditor() {
 
 	return (
 		<>
+			<title>{currentDoc ? `Jaida - ${currentDoc.data()?.displayName}` : 'Jaida'}</title>
 			<main className={s.main}>
 				<Settings
 					setBg={setBg}
---
+++ app/m/[id]/replacements.ts
@@ -0,0 +1,294 @@
+export const replacements = [
+  {
+    active: true,
+    options: [
+      { text: 'Α', active: true },
+      { text: 'α', active: false },
+    ],
+    replaceWith: 'alpha'
+  },
+  {
+    active: true,
+    options: [
+      { text: 'Β', active: true },
+      { text: 'β', active: false },
+    ],
+    replaceWith: 'beta'
+  },
+  {
+    active: true,
+    options: [
+      { text: 'Γ', active: true },
+      { text: 'γ', active: false },
+    ],
+    replaceWith: 'gamma'
+  },
+  {
+    active: true,
+    options: [
+      { text: 'Δ', active: true },
+      { text: 'δ', active: false },
+    ],
+    replaceWith: 'delta'
+  },
+  {
+    active: true,
+    options: [
+      { text: 'Ε', active: true },
+      { text: 'ε', active: false },
+    ],
+    replaceWith: 'epsilon'
+  },
+  {
+    active: true,
+    options: [
+      { text: 'Ζ', active: true },
+      { text: 'ζ', active: false },
+    ],
+    replaceWith: 'zeta'
+  },
+  {
+    active: true,
+    options: [
+      { text: 'Η', active: true },
+      { text: 'η', active: false },
+    ],
+    replaceWith: 'eta'
+  },
+  {
+    active: true,
+    options: [
+      { text: 'Θ', active: true },
+      { text: 'θ', active: false },
+    ],
+    replaceWith: 'theta'
+  },
+  {
+    active: true,
+    options: [
+      { text: 'Ι', active: true },
+      { text: 'ι', active: false },
+    ],
+    replaceWith: 'iota'
+  },
+  {
+    active: true,
+    options: [
+      { text: 'Κ', active: true },
+      { text: 'κ', active: false },
+    ],
+    replaceWith: 'kappa'
+  },
+  {
+    active: true,
+    options: [
+      { text: 'Λ', active: true },
+      { text: 'λ', active: false },
+    ],
+    replaceWith: 'lambda'
+  },
+  {
+    active: true,
+    options: [
+      { text: 'Μ', active: true },
+      { text: 'μ', active: false },
+    ],
+    replaceWith: 'mu'
+  },
+  {
+    active: true,
+    options: [
+      { text: 'Ν', active: true },
+      { text: 'ν', active: false },
+    ],
+    replaceWith: 'nu'
+  },
+  {
+    active: true,
+    options: [
+      { text: 'Ξ', active: true },
+      { text: 'ξ', active: false },
+    ],
+    replaceWith: 'xi'
+  },
+  {
+    active: true,
+    options: [
+      { text: 'Ο', active: true },
+      { text: 'ο', active: false },
+    ],
+    replaceWith: 'omicron'
+  },
+  {
+    active: true,
+    options: [
+      { text: 'Π', active: true },
+      { text: 'π', active: false },
+    ],
+    replaceWith: 'pi'
+  },
+  {
+    active: true,
+    options: [
+      { text: 'Ρ', active: true },
+      { text: 'ρ', active: false },
+    ],
+    replaceWith: 'rho'
+  },
+  {
+    active: true,
+    options: [
+      { text: 'Σ', active: true },
+      { text: 'σ', active: false },
+    ],
+    replaceWith: 'sigma'
+  },
+  {
+    active: true,
+    options: [
+      { text: 'Τ', active: true },
+      { text: 'τ', active: false },
+    ],
+    replaceWith: 'tau'
+  },
+  {
+    active: true,
+    options: [
+      { text: 'Υ', active: true },
+      { text: 'υ', active: false },
+    ],
+    replaceWith: 'upsilon'
+  },
+  {
+    active: true,
+    options: [
+      { text: 'Φ', active: true },
+      { text: 'φ', active: false },
+    ],
+    replaceWith: 'phi'
+  },
+  {
+    active: true,
+    options: [
+      { text: 'Χ', active: true },
+      { text: 'χ', active: false },
+    ],
+    replaceWith: 'chi'
+  },
+  {
+    active: true,
+    options: [
+      { text: 'Ψ', active: true },
+      { text: 'ψ', active: false },
+    ],
+    replaceWith: 'psi'
+  },
+  {
+    active: true,
+    options: [
+      { text: 'Ω', active: true },
+      { text: 'ω', active: false },
+    ],
+    replaceWith: 'omega'
+  },
+  {
+    active: true,
+    option: '₀',
+    replaceWith: '_0'
+  },
+  {
+    active: true,
+    option: '⁰',
+    replaceWith: '^0'
+  },
+  {
+    active: true,
+    option: '₁',
+    replaceWith: '_1'
+  },
+  {
+    active: true,
+    option: '¹',
+    replaceWith: '^1'
+  },
+  {
+    active: true,
+    option: '₂',
+    replaceWith: '_2'
+  },
+  {
+    active: true,
+    option: '²',
+    replaceWith: '^2'
+  },
+  {
+    active: true,
+    option: '₃',
+    replaceWith: '_3'
+  },
+  {
+    active: true,
+    option: '³',
+    replaceWith: '^3'
+  },
+  {
+    active: true,
+    option: '₄',
+    replaceWith: '_4'
+  },
+  {
+    active: true,
+    option: '⁴',
+    replaceWith: '^4'
+  },
+  {
+    active: true,
+    option: '₅',
+    replaceWith: '_5'
+  },
+  {
+    active: true,
+    option: '⁵',
+    replaceWith: '^5'
+  },
+  {
+    active: true,
+    option: '₆',
+    replaceWith: '_6'
+  },
+  {
+    active: true,
+    option: '⁶',
+    replaceWith: '!6'
+  },
+  {
+    active: true,
+    option: '₇',
+    replaceWith: '_7'
+  },
+  {
+    active: true,
+    option: '⁷',
+    replaceWith: '^7'
+  },
+  {
+    active: true,
+    option: '₈',
+    replaceWith: '_8'
+  },
+  {
+    active: true,
+    option: '⁸',
+    replaceWith: '^8'
+  },
+  {
+    active: true,
+    option: '₉',
+    replaceWith: '_9'
+  },
+  {
+    active: true,
+    option: '⁹',
+    replaceWith: '^9'
+  },
+]
