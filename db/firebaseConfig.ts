ae0fd0ca2f90d2c5ba79c5ef72a4cd90f36ee9ed
ZeSved <timothy.korell@gmail.com>
2024-10-11 18:24:52 +0200

    added dummy key

--- db/firebaseConfig.ts
+++ db/firebaseConfig.ts
@@ -1,5 +1,5 @@
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "dummy_key",
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
