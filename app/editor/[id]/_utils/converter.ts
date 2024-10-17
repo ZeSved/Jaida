bcb75ce7292978add875f45dc60e94a20e9054a3
ZeSved <timothy.korell@gmail.com>
2024-10-17 21:48:12 +0200

    progress on editor

--- app/editor/[id]/_utils/converter.ts
+++ app/editor/[id]/_utils/converter.ts
@@ -1,5 +1,106 @@
 import style from '../m-d-editor.module.scss'
 import { Dispatch, SetStateAction } from "react"
+import { ClassNames, Elements } from './elementTypes'
+
+// const matches = new Map([
+//   ['bold', new RegExp(/^\*\*\S+\*\*$/)],
+//   ['italic', new RegExp(/^\*\S+\*$/)],
+//   ['lineThrough', new RegExp(/^\~\~\S+\~\~$/)],
+//   ['emptyRow', new RegExp(/^$/)],
+//   ['space', new RegExp(/^\s$/)],
+// ])
+
+// const replacements = new Map([
+//   ['bold', [new RegExp(/^\*\*/), new RegExp(/\*\*$/), '**']],
+//   ['italic', [new RegExp(/^\*/), new RegExp(/\*$/), '*']],
+//   ['lineThrough', [new RegExp(/^\~\~/), new RegExp(/\~\~$/), '~~']],
+// ])
+
+const conver = [
+  {
+    type: 'bold',
+    match: new RegExp(/^\*\*\S+\*\*$/),
+    shouldBeReplaced: true,
+    startReplace: new RegExp(/^\*\*/),
+    endReplace: new RegExp(/\*\*$/),
+    spanInserts: '**'
+  },
+  {
+    type: 'italic',
+    match: new RegExp(/^\*\S+\*$/),
+    shouldBeReplaced: true,
+    startReplace: new RegExp(/^\*/),
+    endReplace: new RegExp(/\*$/),
+    spanInserts: '*'
+  },
+  {
+    type: 'lineThrough',
+    match: new RegExp(/^\~\~\S+\~\~$/),
+    shouldBeReplaced: true,
+    startReplace: new RegExp(/^\~\~/),
+    endReplace: new RegExp(/\~\~$/),
+    spanInserts: '~~'
+  },
+  {
+    type: 'underLine',
+    match: new RegExp(/^\_\S+\_$/),
+    shouldBeReplaced: true,
+    startReplace: new RegExp(/^\_/),
+    endReplace: new RegExp(/\_$/),
+    spanInserts: '_'
+  },
+  {
+    type: 'overLine',
+    match: new RegExp(/^\¨\S+\¨$/),
+    shouldBeReplaced: true,
+    startReplace: new RegExp(/^\¨/),
+    endReplace: new RegExp(/\¨$/),
+    spanInserts: '¨'
+  },
+]
+
+// class Convert {
+//   elements: HTMLElement[]
+//   conversion: ClassNames
+
+//   constructor(elmType: Elements, conversion: ClassNames) {
+//     this.elements = [document.createElement(elmType)]
+//     this.conversion = conversion
+
+//     this.elements[0].classList.add(style[this.conversion])
+//   }
+
+//   // assignClass(name: ClassNames) {
+//   //   this.element.classList.add(style[name])
+//   //   return this
+//   // }
+
+//   addText(text: string) {
+//     if (this.conversion === 'bold' || this.conversion === 'italic' || this.conversion === 'lineThrough') {
+//       const [startReplace, endReplace, insert] = replacements.get(this.conversion)!
+//       text.replace(startReplace, '').replace(endReplace, '')
+//       const textNode = document.createTextNode(text)
+//       const spans = [document.createElement('span'), document.createElement('span')]
+
+//       spans.forEach(span => {
+//         const textNode = document.createTextNode((insert as string))
+
+//         span.style.opacity = '0.5'
+//         span.appendChild(textNode)
+//       })
+
+//       this.elements.push(spans[0])
+//       this.elements.unshift(spans[1])
+
+//       this.elements[1].appendChild(textNode)
+//     } else {
+//       const textNode = document.createTextNode(text)
+//       this.elements[0].appendChild(textNode)
+//     }
+
+//     return this
+//   }
+// }
 
 const conversions = [
   {
@@ -17,10 +118,21 @@ const conversions = [
     condition: (text: string) => { return /^\*\S+\*$/.test(text) },
     function: (text: string) => {
       const span = document.createElement('span')
+      const edgeSpan1 = document.createElement('span')
+      const edgeSpan2 = document.createElement('span')
+
+      edgeSpan1.appendChild(document.createTextNode('*'))
+      edgeSpan2.appendChild(document.createTextNode('*'))
+
+      edgeSpan1.style.opacity = '0.5'
+      edgeSpan2.style.opacity = '0.5'
+
       span.style.fontStyle = 'italic'
+      span.appendChild(edgeSpan1)
       span.appendChild(document.createTextNode(text.replace(/^\*/, '').replace(/\*$/, '')))
+      span.appendChild(edgeSpan2)
       span.classList.add(style.italic)
-      span.textContent = `*${span.textContent}*`
+      // span.textContent = `${edgeSpan1}${span.textContent}${edgeSpan2}`
 
       // return span.outerHTML
       return span
@@ -90,14 +202,46 @@ export function converter(text: string, setCurrentText: Dispatch<SetStateAction<
         })
       }
 
-      for (const convert of conversions) {
-        if (convert.condition(t)) {
-          // newArray.push(convert.function(t))
-          div.appendChild(convert.function(t))
+      // for (const convert of conversions) {
+      //   if (convert.condition(t)) {
+      //     // newArray.push(convert.function(t))
+      //     div.appendChild(convert.function(t))
 
-          break
+      //     break
+      //   }
+      // }
+      t.split(' ').forEach(r => {
+        for (const convert of conver) {
+          console.log(convert.match.test(r))
+          console.log(r)
+          if (convert.match.test(r)) {
+            const span = document.createElement('span')
+            const span1 = document.createElement('span')
+            const span2 = document.createElement('span')
+
+            span1.textContent = convert.spanInserts
+            span2.textContent = convert.spanInserts
+
+            span1.style.opacity = '0.5'
+            span2.style.opacity = '0.5'
+
+            span.appendChild(span1)
+            span.appendChild(document.createTextNode(r.replace(convert.startReplace, '').replace(convert.endReplace, '')))
+            span.appendChild(span2)
+            span.classList.add(style[convert.type])
+            // newArray.push(convert.function(t))
+            div.appendChild(span)
+
+            break
+          } else if (!/^(\¨\S+\¨|\_\S+\_|\~\~\S+\~\~|\*\S+\*|\*\*\S+\*\*)*$/.test(r)) {
+            const span = document.createElement('span')
+            span.appendChild(document.createTextNode(r))
+            div.appendChild(span)
+
+            break
+          }
         }
-      }
+      })
     })
 
     newArray.push(div.outerHTML)
